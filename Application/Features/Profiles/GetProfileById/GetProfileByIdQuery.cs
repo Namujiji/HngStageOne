0d0a30a604b466f1d49ca7fb9810b7a71f0b7418
Abdullahi Shuru <abdullahishuru@gmail.com>
2026-04-20 14:37:59 +0100

    feat: implement get profile by id

---
+++ Application/Features/Profiles/GetProfileById/GetProfileByIdQuery.cs
@@ -0,0 +1,25 @@
+﻿using Cortex.Mediator.Queries;
+using HngStageOne.Application.Features.Profiles.Responses;
+using HngStageOne.Application.Utilities;
+using HngStageOne.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace HngStageOne.Application.Features.Profiles.GetProfileById;
+
+public sealed record GetProfileByIdQuery(Guid Id) : IQuery<Result<IProfileResponse>>;
+public sealed record GetProfileByIdQueryHandler(
+    ProfileDbContext DbContext,
+    ILogger<GetProfileByIdQueryHandler> Logger) : IQueryHandler<GetProfileByIdQuery, Result<IProfileResponse>>
+{
+    public async Task<Result<IProfileResponse>> Handle(GetProfileByIdQuery query, CancellationToken cancellationToken)
+    {
+        var profile = await DbContext.Profiles.FirstOrDefaultAsync(p => p.Id == query.Id, cancellationToken);
+
+        if (profile is null)
+        {
+            return await Result<IProfileResponse>.FailureAsync(new ErrorResponse("error", "Profile not found"));
+        }
+
+        return await Result<IProfileResponse>.SuccessAsync(new SuccessResponse("success", ProfileResponse.FromProfile(profile)));
+    }
+}
--- Controllers/Profiles/ProfilesController.cs
+++ Controllers/Profiles/ProfilesController.cs
@@ -1,10 +1,9 @@
 ﻿using Cortex.Mediator;
-using HngStageOne.Application.Features.Genderize.GetGenderByName;
 using HngStageOne.Application.Features.Profiles.CreateProfile;
+using HngStageOne.Application.Features.Profiles.GetProfileById;
 using HngStageOne.Application.Features.Profiles.Responses;
 using HngStageOne.Controllers.Profiles.Requests;
 using Microsoft.AspNetCore.Mvc;
-using System.ComponentModel.DataAnnotations;
 
 namespace HngStageOne.Controllers.Profiles;
 
@@ -44,31 +43,31 @@ public class ProfilesController(IMediator mediator) : ControllerBase
         }
     }
 
-    //[HttpGet("{id:guid}")]
-    //[Produces("application/json")]
-    //[ProducesResponseType(typeof(SuccessResponse), StatusCodes.Status200OK)]
-    //[ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
-    //public async Task<IActionResult> Create(Guid id, CancellationToken cancellationToken)
-    //{
-    //    var result = await mediator.SendCommandAsync(new GetProfileByIdCommand(id), cancellationToken);
+    [HttpGet("{id:guid}")]
+    [Produces("application/json")]
+    [ProducesResponseType(typeof(SuccessResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> GetById(Guid id, CancellationToken cancellationToken)
+    {
+        var result = await mediator.SendQueryAsync(new GetProfileByIdQuery(id), cancellationToken);
 
-    //    try
-    //    {
-    //        if (result.Succeeded)
-    //        {
-    //            return CreatedAtAction(nameof(Create), result.Data);
-    //        }
+        try
+        {
+            if (result.Succeeded)
+            {
+                return Ok(result.Data);
+            }
 
-    //        if (result.Error!.Status == "502")
-    //        {
-    //            return StatusCode(StatusCodes.Status502BadGateway, result.Data);
-    //        }
+            if (result.Error!.Status == "502")
+            {
+                return StatusCode(StatusCodes.Status502BadGateway, result.Error);
+            }
 
-    //        return BadRequest(result.Data);
-    //    }
-    //    catch (Exception)
-    //    {
-    //        return StatusCode(StatusCodes.Status500InternalServerError, result.Data);
-    //    }
-    //}
+            return BadRequest(result.Error);
+        }
+        catch (Exception)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, result.Error);
+        }
+    }
 }
